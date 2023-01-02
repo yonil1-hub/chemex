ec977252051909acc955fc7119bd683a7f1231f9
Jamescog <jamescog72@gmail.com>
2023-01-02 22:05:29 +0300

    homepage recent part taken care

--- src/front-end/home.js
+++ src/front-end/home.js
@@ -1,33 +1,72 @@
 $(document).ready(function () {
-  //handle homepage
+  // This function will be executed when the document is ready
 
-  // get recent posts
+  // get recent posts from the server
   $.ajax({
     type: "GET",
     url: "http://127.0.0.1:5000/api/v1/recent",
     success: function (response) {
+      // This function will be executed if the request succeeds
       let obj = Object.values(response.msg);
 
-      //fill all the titles
+      // Loop through all cards and add postId attribute
       let num = 0;
+      $(".card").each(function () {
+        let postId = obj[num].postId;
+        $(this).attr("postId", postId);
+        num += 1;
+      });
+
+      // Loop through all the cards and fill the category
+      num = 0;
       $(".card h5").each(function () {
         let category = obj[num].category;
         $(this).text(category);
         num += 1;
       });
-      // fill the descriptions
+
+      // Loop through all the cards and fill the title
       num = 0;
       $(".card p").each(function () {
-        let desc = obj[num].description;
+        let desc = obj[num].title;
         $(this).text(desc);
         num += 1;
       });
+
+      // Loop through all the cards and fill the time
+      num = 0;
+      $(".card #duration").each(function () {
+        let duraton = obj[num].duration;
+        $(this).text(duraton);
+        num += 1;
+      });
+
+      // Add a "Read More" button to the card when the user hovers over it
+      $(".card").hover(
+        function () {
+          $(this)
+            .find(".card-footer")
+            .append(
+              '<button class="btn btn-primary" id="Read">Read More</button>'
+            );
+        },
+        function () {
+          // Remove the button when the user moves the mouse away from the card
+          $(this).find("#Read").remove();
+        }
+      );
+    },
+    error: function () {
+      // This function will be executed if the request fails
+      // do something
     },
   });
-  // $("#readmore1").click(function (e) {
-  //   e.preventDefault();
-  //   postId = 16;
-  //   userId = 1;
-  //   window.location.href = "post.html?postId=" + postId + "&userId=" + userId;
-  // });
+
+  // Add a click event handler for the "Read More" button
+  $(document).on("click", "#Read", function () {
+    let card = $(this).closest(".card");
+    let postId = card.attr("postId");
+    let userId = 2;
+    window.location.href = "post.html?postId=" + postId + "&userId=" + userId;
+  });
 });
